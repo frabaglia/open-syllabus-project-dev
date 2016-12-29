02272a7ebb24f274c260aa747a0830b315dfde8b
Franco Rabaglia <francoo.hd@gmail.com>
2016-12-29 12:13:45 -0300

    Build

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ var PORT = process.env.PORT || 80
 app.use(express.static(path.join(__dirname + '/client/build/.')));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/client/build/index.html')
+    response.sendFile(__dirname + '/client/build/index.html')
 });
 
 app.listen(PORT, function(error) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  }
+
+    if (error) {
+        console.error(error);
+    } else {
+        console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    }
+
 });
